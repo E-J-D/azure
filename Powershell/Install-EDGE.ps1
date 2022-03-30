d0b31e983b4044b1337a878ab74381a6b4975b2f
E-J-D <github@doose.email>
2022-03-30 09:18:30 +0200

    .

--- Powershell/Install-EDGE.ps1
+++ Powershell/Install-EDGE.ps1
@@ -1,4 +1,5 @@
 # 23.03.2022 Eike Doose eike.doose@starke.de
+# curl.exe https://raw.githubusercontent.com/E-J-D/sdms-cloud1-azure/main/Powershell/Install-EDGE.ps1 --output Install-EDGE.ps1
 md -Path $env:temp\edgeinstall -erroraction SilentlyContinue | Out-Null
 $Download = join-path $env:temp\edgeinstall MicrosoftEdgeEnterpriseX64.msi
 Invoke-WebRequest 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a2662b5b-97d0-4312-8946-598355851b3b/MicrosoftEdgeEnterpriseX64.msi'  -OutFile $Download
