cabb0b4efbd6032c6272c6648797c3403b219eec
E-J-D <github@doose.email>
2022-03-31 15:40:01 +0200

    .

--- Powershell/Install-Starke-DMS.ps1
+++ Powershell/Install-Starke-DMS.ps1
@@ -1,4 +1,4 @@
-# 28.03.2022 Eike Doose / licenced commerical use only - do not distribute
+# 31.03.2022 Eike Doose / licenced commerical use only - do not distribute
 # ========================================================================
 New-Item -Name "install\StarkeDMS-latest" -ItemType Directory -Path "C:\"
 curl.exe "ftp://get--it:get--IT2022@ftp.get--it.de/StarkeDMSlatest.zip" --output C:\install\StarkeDMS-latest\StarkeDMSlatest.zip
@@ -10,5 +10,7 @@ Remove-Item C:\install\StarkeDMS-latest\ABBYYlatest.zip
 curl.exe "https://raw.githubusercontent.com/E-J-D/sdms-cloud1-azure/main/Powershell/Install-Starke-DMS_setup.reg" --output C:\install\StarkeDMS-latest\setup.reg
 reg import C:\install\StarkeDMS-latest\setup.reg /reg:32
 # reg import https://github.com/E-J-D/sdms-cloud1-azure/blob/main/Powershell/Install-Starke-DMS_setup.reg /reg:32
+Get-ChildItem -Path C:\install\StarkeDMS-latest\* -Include ABBYY*.exe | Rename-Item -NewName ABBYY-latest.exe
+# C:\install\StarkeDMS-latest\ABBYY-latest.exe
 Get-ChildItem -Path C:\install\StarkeDMS-latest\* -Include StarkeDMS*.exe | Rename-Item -NewName StarkeDMS-latest.exe
 C:\install\StarkeDMS-latest\StarkeDMS-latest.exe /S
