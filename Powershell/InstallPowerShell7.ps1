8d057526d6d9f34cf939001321bcd1389c4d4bfb
E-J-D <github@doose.email>
2022-04-01 23:37:03 +0200

    .

--- Powershell/InstallPowerShell7.ps1
+++ Powershell/InstallPowerShell7.ps1
@@ -1,4 +1,5 @@
+# 01.04.2022 Eike Doose
 # https://docs.microsoft.com/de-de/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.2
 
-curl.exe "https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/PowerShell-7.2.2-win-x64.msi" --output PowerShell-7.2.2-win-x64.msi
-msiexec.exe /package PowerShell-7.2.2-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1
+curl.exe "ftp://get--it:get--IT2022@ftp.get--it.de/PowerShell-7.2.2-win-x64.msi" --output C:\install\StarkeDMS-latest\PowerShell-7.2.2-win-x64.msi --create-dirs
+Start-Process -wait C:\install\StarkeDMS-latest\PowerShell-7.2.2-win-x64.msi -ArgumentList "/quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1"
