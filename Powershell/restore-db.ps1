be9a5fb11f1fa8732d981c0c9781e98331737939
E-J-D <github@doose.email>
2022-04-19 14:30:51 +0200

    .

---
+++ Powershell/restore-db.ps1
@@ -0,0 +1,29 @@
+$SQLInstance = "localhost\SDMSCLOUD1"
+#$Date = Get-Date -format yyyyMMdd
+#Backup-SqlDatabase  -ServerInstance $SQLInstance `
+#                    -Database $DBName `
+#                    -CopyOnly `
+#                    -CompressionOption on `
+#                    -BackupFile "$($SharedFolder)\$DBName-$date.bak" `
+#                    -BackupAction Database `
+#                    -checksum ` 
+#                    -verbose
+
+$DBName = "50999"
+$Backupfile = "C:\install\SQL-DB-CLOUD1MASTER1.bak"
+#Restore-SqlDatabase  -ServerInstance $SQLInstance -Database "$DbNAme" -BackupFile "$Backupfile" -verbose -AutoRelocateFile
+
+Restore-SqlDatabase -ServerInstance $SQLInstance `
+-Database "$DbNAme" `
+-BackupFile "$Backupfile" `
+-verbose `
+-AutoRelocateFile
+
+
+#-RestoreAction Database |
+
+#					 -ReplaceDatabase |
+					  ;
+#					 -RelocateFile |
+#					 -SqlCredential |
+#					;
