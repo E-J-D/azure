04bd42f5d87fedf2ca2d423b1d975ccf9d8eda30
E-J-D <github@doose.email>
2022-11-10 16:57:02 +0100

    .
    ..

--- Powershell/AutoUpdate.ps1
+++ Powershell/AutoUpdate.ps1
@@ -4,9 +4,19 @@
 # https://www.itechtics.com/run-windows-update-cmd/
 
 # Install PSWindowsUpdate Modul for PowerShell
+Write-Host 
+Write-Host -ForegroundColor Yellow "################################################"
+Write-Host -ForegroundColor Yellow "# Install PSWindowsUpdate Modul for PowerShell #"
+Write-Host -ForegroundColor Yellow "################################################"
+Write-Host
 pause
 Install-Module -Name PSWindowsUpdate -Force
 
 # Install all pending Updates and restart without asking
+Write-Host 
+Write-Host -ForegroundColor Yellow "##########################################################"
+Write-Host -ForegroundColor Yellow "# Install all pending Updates and restart without asking #"
+Write-Host -ForegroundColor Yellow "##########################################################"
+Write-Host
 pause
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
