85addab8e5d23909fc968c74d8f05d304869c198
E-J-D <github@doose.email>
2022-12-05 13:26:23 +0100

    .

--- Powershell/Install-Starke-DMS_DBfixLic.ORIG.ps1
+++ Powershell/Install-Starke-DMS_DBfixLic.ORIG.ps1
@@ -1,3 +1,6 @@
+﻿# 07.04.2022 Boris Brixel
+#### .\dms-license-dbupdate.ps1 -sqlserver dbserver.starke.intern -database 'DMSDB' -username 'sa' -password 'geheim' -configpath 'C:\entwicklung\DMSDEV\Exe\config50999_3'
+# C:\install\Install-Starke-DMS_DB2fixLic.ps1 -sqlserver localhost\SDMSCLOUD1 -database $customerno -username 'sa' -password $saPass -configpath $LIZtargetdir
 
 param (
 	[Parameter(Mandatory=$true)][string]$sqlserver,
@@ -7,23 +10,25 @@ param (
 	[Parameter(Mandatory=$true)][string]$configpath
 )
 
-cd "D:\dms-data\sql\Client SDK\ODBC\170\Tools\Binn\"
+#Get-Command -ModuleName 'sqlserver'
+#Invoke-SqlCmd -ServerInstance $sqlserver -Database $database -Query $sqlquery -Username "$username" -Password "$password" -Verbose
 
 $sqlquery = 'SELECT name FROM ArchivPlus.versions WHERE description LIKE ""Datenbank Kundenindividuelle Struktur"";'
-$olduid = .\SQLCMD.EXE -S localhost\SDMSCLOUD1 -d $customerno -Q "$sqlquery" -U 'sa' -P $saPass -h -1
+$olduid = .\SQLCMD.EXE -S "$sqlserver" -d "$database" -Q "$sqlquery" -U "$username" -P "$password" -h -1
 $olduid = ($olduid -split '\r\n')[0]
 $olduid = $olduid.Trim()
 if ($olduid -match '\{.+\}') {
 	$olduid = $Matches.0
 	Write-Host Alte UID: $olduid
 	$file = Get-Childitem –Path "$configpath" -File -ErrorAction SilentlyContinue | Where-Object { $_.Name -match 'Version(\{.+\})\.dat' }
+	#Write-host $file
+	#Write-Host ($Matches.1)
 	if (("$file" -ne '') -And ("$Matches.1" -ne '')) {
 		$newuid = $Matches.1
 		Write-Host Neue UID: $newuid
 		$sqlupdate = "UPDATE ArchivPlus.versions SET name = """"db$newuid"""" WHERE name = """"db$olduid"""";"
 		#Write-Host $sqlupdate
-#		$updateresult = .\SQLCMD.EXE -S "$sqlserver" -d "$database" -Q "$sqlupdate" -U "$username" -P "$password" -h -1
-		$updateresult = .\SQLCMD.EXE -S localhost\SDMSCLOUD1 -d $customerno -Q "$sqlupdate" -U 'sa' -P  $saPass -h -1
+		$updateresult = .\SQLCMD.EXE -S "$sqlserver" -d "$database" -Q "$sqlupdate" -U "$username" -P "$password" -h -1
 		$updateresult = $updateresult.Trim()
 		Write-Host $updateresult
 	} else {
@@ -32,8 +37,3 @@ if ($olduid -match '\{.+\}') {
 } else {
 	Write-Host Alte UID nicht gefunden.
 }
-
-PrintJobDone "fix DB to new customer (fixLic) done"
-Start-Sleep -s 2
-# Clear-Host []
-pause
